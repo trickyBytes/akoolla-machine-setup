a1c3bbb0d5020e85c3df3e22e9ce3bb5576faac9
trickyBytes <richard.tiffin@akoolla.com>
2017-10-02 07:09:16 +0100

    A nicer set-up

--- emacs/init.el
+++ emacs/init.el
@@ -1,41 +1,5 @@
 (require 'package)
-(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
-
-(defvar my-packages '(better-defaults
-                      projectile
-                      clojure-mode
-                      ido-at-point ;; completion with ido
-                      cider))
-
-(package-initialize)
-
-(dolist (p my-packages)
-  (unless (package-installed-p p)
-    (package-install p)))
-
-(ido-at-point-mode)
-
-;; when in programming mode enable eldoc - docs for language
-(add-hook 'prog-mode-hook 'eldoc-mode)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (brin)))
- '(custom-safe-themes (quote ("0c29db826418061b40564e3351194a3d4a125d182c6ee5178c237a7364f0ff12" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" default)))
- '(menu-bar-mode t)
- '(org-agenda-files nil)
- '(package-archives (quote (("marmalade" . "http://marmalade-repo.org/packages/") ("gnu" . "http://elpa.gnu.org/packages/") ("Mepla" . "http://melpa.milkbox.net/packages/"))))
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
 
 (defvar my-packages '(better-defaults
